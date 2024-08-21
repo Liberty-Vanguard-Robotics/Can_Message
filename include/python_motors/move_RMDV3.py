30a5588448b21e0379d76abcb38f8dfc23e2a982
LARagan1 <158233377+LARagan1@users.noreply.github.com>
2024-08-21 09:20:06 -0400

    Added motor stopping command

--- include/python_motors/move_RMDV3.py
+++ include/python_motors/move_RMDV3.py
@@ -1,5 +1,6 @@
 import os
 import can
+import time
 
 #Set up the can interfaces
 # Notice that the bitrate is set to 1Mbps. This is different between the V2 and V3 RMD motors
@@ -44,6 +45,18 @@ print("Motor Angle is " + str((response[7] << 4) +response[6]) + " degrees")
 
 # response_multi = can0.recv(10.0)
 # print(response_multi)
+
+#Reset the speed to 0
+time.sleep(5) #Delay 5 seconds before stopping the motor
+speed_0_msg = can.Message(is_extended_id=False,arbitration_id=single_id,data = [0xA2,0x00,0x00,0x00,0x00,0x00,0x00,0x00])
+
+can0.send(speed_0_msg)
+
+response = can0.recv(10.0)
+print("Motor temperature is " + str(response[1]) + " C")
+print("Torque current is " + str(((response[3] << 4) +response[2])*0.01) + " A")
+print("Motor speed is " + str((response[5] << 4) +response[4]) + " dps")
+print("Motor Angle is " + str((response[7] << 4) +response[6]) + " degrees")
 
 #Shutdown CAN interfaces
 os.system('sudo ifconfig can0 down')
