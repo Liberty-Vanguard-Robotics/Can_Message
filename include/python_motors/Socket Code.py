529c8b32bafdbce60923eceaccc88facce7ea30c
Tyler Sackel <78436840+BiziLife@users.noreply.github.com>
2024-11-14 19:54:16 -0500

    updated code

--- include/python_motors/Socket Code.py
+++ include/python_motors/Socket Code.py
@@ -1,17 +1,18 @@
-import socket
+import socket  # This is now safe to use as we're not importing our own script named `socket`
 import pickle
-import test2_gather #the file where the data we're sending comes from
+import test3_gather  # Import your module with the data
 
 def send_command(data):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('192.168.168.206', 65432))
 
-    #serializes the array to be able to send
+    # Serialize the array to send
     serialized_data = pickle.dumps(data)
-
 
     client_socket.sendall(serialized_data)
     client_socket.close()
     print('Data Sent')
 
-send_command(test2_gather.array_matrix_print) #Sending the entire array through in a single command
+# Ensure `array_matrix_print` is properly defined in `test3_gather.py`
+data_to_send = test3_gather.main()  # If `main()` returns `array_matrix_print`
+send_command(data_to_send)  # Sending the array through in a single command
