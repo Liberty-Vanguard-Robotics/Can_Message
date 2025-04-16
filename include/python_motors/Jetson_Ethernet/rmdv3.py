1a0ca8f94fa441d49bc209978b48640196bf0ca4
CalebMarcellus <cmarcellus1@liberty.edu>
2025-04-15 21:02:05 -0400

    hjhgj

--- include/python_motors/Jetson_Ethernet/rmdv3.py
+++ include/python_motors/Jetson_Ethernet/rmdv3.py
@@ -97,8 +97,9 @@ def increasing_speed_set(motor_id, axis_value, max_speed,bits_length=32):
 
 
 
-def twos_comp(val, bits):
-    """compute the 2's complement of int value val"""
+def twos_comp(val: int, bits: int) -> int:
+    """compute the 2's complement of int value val
+    """
     if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
         val = val - (1 << bits)        # compute negative value
     return val
